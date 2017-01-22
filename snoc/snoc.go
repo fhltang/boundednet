4f332604e7c6a396ab3d6f436af417e05da80af3
Francis Tang <francis.tang@gmail.com>
2017-01-21 19:40:28 -0800

    Move snoc-recursive solution into its own module.
    Update documentation and tests.

---
+++ snoc/snoc.go
@@ -0,0 +1,167 @@
+// A snoc-recursive solution of the bounded network problem.
+
+package snoc
+
+import (
+	bn "github.com/fhltang/boundednet"
+	"sort"
+)
+
+type TableCell struct {
+	// Size of a minimal solution.
+	MinSize int
+
+	// A minimal solution is obtained by combining Network with a
+	// subsolution (NextRow, NextCol).
+	//
+	// Used by backtracking algorithm.
+	NextRow int
+	NextCol int
+	Network bn.Network
+}
+
+type BacktrackingSolver struct {
+	// Inputs.
+	Input []bn.Network
+	M     int
+
+	// Precomputed values of LeastNetwork().
+	leastNetwork [][]bn.Network
+
+	// Table used in dynamic programming solution.
+	Table [][]TableCell
+}
+
+func (this *BacktrackingSolver) Solve(input []bn.Network, m int) []bn.Network {
+	this.Init(input, m)
+	this.PrecomputeLeastNetwork()
+	this.ComputeTable()
+	return this.Backtrack(this.M, len(this.Input))
+}
+
+type ByLeftWidth []bn.Network
+
+func (this ByLeftWidth) Len() int      { return len(this) }
+func (this ByLeftWidth) Swap(i, j int) { this[i], this[j] = this[j], this[i] }
+func (this ByLeftWidth) Less(i, j int) bool {
+	if this[i].Left == this[j].Left {
+		return this[i].Right > this[j].Right
+	}
+	return this[i].Left < this[j].Left
+}
+
+func (this *BacktrackingSolver) Init(input []bn.Network, m int) {
+	this.Input = make([]bn.Network, len(input))
+	copy(this.Input, input)
+
+	// Sort input.
+	sort.Sort(ByLeftWidth(this.Input))
+
+	// Remove overlapping networks.
+	i := 0
+	for j := 0; j < len(this.Input)-1; j++ {
+		if this.Input[j].Right <= this.Input[j+1].Left {
+			if i < j {
+				this.Input[i+1] = this.Input[j+1]
+			}
+			i++
+		}
+	}
+	this.Input = this.Input[:i+1]
+
+	this.M = m
+}
+
+func (this *BacktrackingSolver) LeastNetwork(i, j int) bn.Network {
+	return this.leastNetwork[j][i]
+}
+
+func (this *BacktrackingSolver) PrecomputeLeastNetwork() {
+	this.leastNetwork = make([][]bn.Network, 0, len(this.Input)+1)
+	for j := 0; j <= len(this.Input); j++ {
+		this.leastNetwork = append(
+			this.leastNetwork, make([]bn.Network, j+1))
+		for i := 0; i <= j; i++ {
+			this.leastNetwork[j][i] = this.computeLeastNetwork(i, j)
+		}
+	}
+}
+
+func (this BacktrackingSolver) computeLeastNetwork(i, j int) bn.Network {
+	if i == j {
+		return bn.EmptyNetwork()
+	}
+
+	left := this.Input[i].ToNonEmptyNetwork()
+	right := this.Input[j-1].ToNonEmptyNetwork()
+	for left != right {
+		if right.K < left.K {
+			right = bn.NonEmptyNetwork{A: right.A >> 1, K: right.K + 1}
+		} else if left.K < right.K {
+			left = bn.NonEmptyNetwork{A: left.A >> 1, K: left.K + 1}
+		} else if left.A < right.A {
+			right = bn.NonEmptyNetwork{A: right.A >> 1, K: right.K + 1}
+		} else if right.A < left.A {
+			left = bn.NonEmptyNetwork{A: left.A >> 1, K: left.K + 1}
+		}
+	}
+	return left.ToNetwork()
+}
+
+func (this *BacktrackingSolver) ComputeTable() {
+	this.Table = make([][]TableCell, this.M)
+	for m := 0; m < this.M; m++ {
+		this.Table[m] = make([]TableCell, len(this.Input))
+		for k := 0; k < len(this.Input); k++ {
+			this.Table[m][k] = this.computeCell(m, k)
+		}
+	}
+}
+
+func (this BacktrackingSolver) computeCell(m, k int) TableCell {
+	if m == 0 {
+		network := this.LeastNetwork(0, k+1)
+		return TableCell{
+			MinSize: network.Size(),
+			Network: network,
+		}
+	}
+
+	minimalSolution := TableCell{MinSize: 1 << 32} // max
+	for n := 0; n <= k; n++ {
+		network := this.LeastNetwork(n+1, k+1)
+		presolutionSize := network.Size() + this.Table[m-1][n].MinSize
+		if presolutionSize < minimalSolution.MinSize {
+			minimalSolution = TableCell{
+				MinSize: presolutionSize,
+				Network: network,
+				NextRow: m - 1,
+				NextCol: n,
+			}
+		}
+	}
+	return minimalSolution
+}
+
+func (this *BacktrackingSolver) Backtrack(m, n int) []bn.Network {
+	output := make([]bn.Network, m)
+	head := m
+	row, col := m-1, n-1
+	for {
+		cell := this.Table[row][col]
+		if cell.Network.Size() > 0 {
+			head--
+			output[head] = cell.Network
+		}
+		if row == 0 {
+			break
+		}
+		row, col = cell.NextRow, cell.NextCol
+	}
+	return output[head:]
+}
+
+func Solve(input []bn.Network, m int) []bn.Network {
+	solver := BacktrackingSolver{}
+	return solver.Solve(input, m)
+}
---
+++ snoc/snoc_test.go
@@ -0,0 +1,211 @@
+package snoc_test
+
+import (
+	"fmt"
+	bn "github.com/fhltang/boundednet"
+	"github.com/fhltang/boundednet/snoc"
+	"reflect"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	type Case struct {
+		Name     string
+		Input    []bn.Network
+		Expected []bn.Network
+	}
+	cases := []Case{
+		{
+			"SortedNoOverlap",
+			[]bn.Network{
+				bn.Network{0, 1},
+				bn.Network{1, 2},
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+			},
+			[]bn.Network{
+				bn.Network{0, 1},
+				bn.Network{1, 2},
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+			},
+		},
+		{
+			"SortedWithFullOverlap",
+			[]bn.Network{
+				bn.Network{0, 1},
+				bn.Network{0, 2},
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+			},
+			[]bn.Network{
+				bn.Network{0, 2},
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+			},
+		},
+		{
+			"UnsortedNoOverlap",
+			[]bn.Network{
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+				bn.Network{0, 1},
+				bn.Network{1, 2},
+			},
+			[]bn.Network{
+				bn.Network{0, 1},
+				bn.Network{1, 2},
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+			},
+		},
+		{
+			"UnsortedWithOverlap",
+			[]bn.Network{
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+				bn.Network{35, 36},
+				bn.Network{0, 1},
+				bn.Network{1, 2},
+			},
+			[]bn.Network{
+				bn.Network{0, 1},
+				bn.Network{1, 2},
+				bn.Network{32, 36},
+				bn.Network{60, 64},
+			},
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			solver := snoc.BacktrackingSolver{}
+			solver.Init(tc.Input, 1)
+			if !reflect.DeepEqual(tc.Expected, solver.Input) {
+				t.Error("Expected", tc.Expected,
+					"got", solver.Input)
+			}
+		})
+	}
+
+}
+
+func TestPrecomputeLeastNetwork(t *testing.T) {
+	input := []bn.Network{
+		bn.Network{0, 1},
+		bn.Network{1, 2},
+		bn.Network{32, 36},
+		bn.Network{60, 64},
+	}
+
+	solver := snoc.BacktrackingSolver{}
+	solver.Init(input, 1)
+	solver.PrecomputeLeastNetwork()
+
+	type Case struct {
+		Left     int
+		Right    int
+		Expected bn.Network
+	}
+
+	cases := []Case{
+		{0, 1, bn.Network{0, 1}},
+		{0, 2, bn.Network{0, 2}},
+		{0, 3, bn.Network{0, 64}},
+		{0, 4, bn.Network{0, 64}},
+		{2, 4, bn.Network{32, 64}},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("L=%d, R=%d", tc.Left, tc.Right),
+			func(t *testing.T) {
+				n := solver.LeastNetwork(tc.Left, tc.Right)
+				if tc.Expected != n {
+					t.Error("Expecting", tc.Expected,
+						"got", n)
+				}
+			})
+	}
+}
+
+func TestComputeTable(t *testing.T) {
+	input := []bn.Network{
+		bn.Network{0, 1},
+		bn.Network{1, 2},
+		bn.Network{32, 36},
+		bn.Network{60, 64},
+	}
+
+	solver := snoc.BacktrackingSolver{}
+	solver.Init(input, 4)
+	solver.PrecomputeLeastNetwork()
+	solver.ComputeTable()
+
+	type Case struct {
+		N               int
+		M               int
+		ExpectedMinSize int
+	}
+
+	cases := []Case{
+		{4, 4, 10},
+		{2, 1, 2},
+		{3, 2, 6},
+		{4, 2, 34},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("N=%d M=%d", tc.M, tc.N), func(t *testing.T) {
+			r, c := tc.M-1, tc.N-1
+			minSize := solver.Table[r][c].MinSize
+			if tc.ExpectedMinSize != minSize {
+				t.Error("Expecting MinSize ",
+					tc.ExpectedMinSize, " got ", minSize)
+			}
+		})
+
+	}
+}
+
+func TestBacktrack(t *testing.T) {
+	input := []bn.Network{
+		bn.Network{0, 1},
+		bn.Network{1, 2},
+		bn.Network{32, 36},
+		bn.Network{60, 64},
+	}
+
+	solver := snoc.BacktrackingSolver{}
+	solver.Init(input, 4)
+	solver.PrecomputeLeastNetwork()
+	solver.ComputeTable()
+
+	type Case struct {
+		M        int
+		N        int
+		Expected []bn.Network
+	}
+	cases := []Case{
+		{1, 4, []bn.Network{
+			bn.Network{0, 64},
+		}},
+		{2, 4, []bn.Network{
+			bn.Network{0, 2},
+			bn.Network{32, 64},
+		}},
+		{3, 4, []bn.Network{
+			bn.Network{0, 2},
+			bn.Network{32, 36},
+			bn.Network{60, 64},
+		}},
+	}
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("M=%d", tc.M), func(t *testing.T) {
+			solution := solver.Backtrack(tc.M, tc.N)
+			if !reflect.DeepEqual(tc.Expected, solution) {
+				t.Error("Expected", tc.Expected,
+					"got", solution)
+			}
+		})
+
+	}
+}
